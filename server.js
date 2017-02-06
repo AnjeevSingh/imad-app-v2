ea1f19540694bf94e86b043d681e27a9dd2352e5
AnjeevSingh <anjeevsingh@gmail.com>
2017-02-07 00:28:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 app.get('/MyMarket', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'MyMarket.html'));
+   res.sendFile(path.join(__dirname, 'ui', 'mymarket.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
